68ad188abf8cfe7d64872ace30e9e887d3cd44c3
lutfiqaraman <lutfiqaraman@gmail.com>
2021-03-05 20:49:21 +0200

    Insert A Row of data

--- DemoApp/Contact.aspx.cs
+++ DemoApp/Contact.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +18,27 @@ namespace DemoApp
 
         protected void BtnSend_Click(object sender, EventArgs e)
         {
+            InsertData(cfMsg.Name, cfMsg.Msg);
             lblOutput.Text = string.Format("User: {0} said: {1}", cfMsg.Name, cfMsg.Msg);
+        }
+
+        protected void InsertData(string Name, string Msg)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MsgDB"].ConnectionString;
+            string insertRecord = "INSERT into Msgs (Name, MSg) values (@Name, @Msg)";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+
+                using (var sqlCommand = new SqlCommand(insertRecord, sqlConnection))
+                {
+                    sqlCommand.Parameters.AddWithValue("Name", Name);
+                    sqlCommand.Parameters.AddWithValue("Msg", Msg);
+
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
